a6c7fe0abf46b6c353a60a40787d2d2c85a30a8f
Alesya <rabushka2014@mail.ru>
2023-12-04 13:53:08 +0300

    main interface

--- client/src/components/chatComponent/chatComponent.tsx
+++ client/src/components/chatComponent/chatComponent.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chatComponent.css"
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import SendIcon from '@mui/icons-material/Send';
 
 export const ChatComponent = () => {
+    const [textValue, setTextValue] = useState('')
+
+
     return (
         <div className="chat-component">
+            <div className="dialogue-field">
+
+            </div>
+            <div className="input-form">
+                <input className="input-field" 
+                    placeholder="Введите текст здесь..."
+                    onChange={e => setTextValue(e.target.value)}
+                    value={textValue}
+                />
+                <div className="menu-button">
+                    <SendIcon fontSize="large" className="send-icon"/>
+                </div>
+                <QuestionMarkIcon fontSize="large" className="helper"/>
+            </div>
         </div>
     )
 }
